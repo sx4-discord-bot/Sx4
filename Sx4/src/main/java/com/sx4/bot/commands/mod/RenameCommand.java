1d3b8c2636b400de3037fdb46daf3a54b7e86c28
Shea4 <sc4gaming@gmail.com>
2021-03-06 14:59:44 +0000

    Allow no argument for nick in rename to reset nickname

--- Sx4/src/main/java/com/sx4/bot/commands/mod/RenameCommand.java
+++ Sx4/src/main/java/com/sx4/bot/commands/mod/RenameCommand.java
@@ -13,12 +13,13 @@ public class RenameCommand extends Sx4Command {
 		super("rename", 252);
 
 		super.setDescription("Rename a member in the server");
+		super.setAliases("nick", "nickname");
 		super.setExamples("rename @Shea#6653 tjej", "rename lakrits e-girl hunter", "rename tjej");
 		super.setBotDiscordPermissions(Permission.NICKNAME_CHANGE);
 		super.setAuthorDiscordPermissions(Permission.NICKNAME_CHANGE);
 	}
 
-	public void onCommand(Sx4CommandEvent event, @Argument(value="user", nullDefault=true) Member member, @Argument(value="nickname", endless=true) @Limit(max=32) String nick) {
+	public void onCommand(Sx4CommandEvent event, @Argument(value="user", nullDefault=true) Member member, @Argument(value="nickname", endless=true, nullDefault=true) @Limit(max=32) String nick) {
 		if (member != null && member.canInteract(event.getMember())) {
 			event.replyFailure("You cannot change the nickname of someone higher or equal than your top role").queue();
 			return;
@@ -27,7 +28,7 @@ public class RenameCommand extends Sx4Command {
 		Member effectiveMember = member == null ? event.getMember() : member;
 
 		event.getGuild().modifyNickname(effectiveMember, nick)
-			.flatMap($ -> event.replySuccess("Renamed " + effectiveMember.getAsMention() + " to **" + nick + "**"))
+			.flatMap($ -> event.replySuccess("Renamed " + effectiveMember.getAsMention() + " to **" + (nick == null ? effectiveMember.getEffectiveName() : nick) + "**"))
 			.queue();
 	}
 
