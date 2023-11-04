68c5f5a8249553f15ec157556b12cc7965f16e97
Shea4 <sc4gaming@gmail.com>
2023-11-04 15:07:27 +0000

    Remove AcceptNull from both or and and formatter functions

--- Sx4/src/main/java/com/sx4/bot/formatter/output/parser/OrFunction.java
+++ Sx4/src/main/java/com/sx4/bot/formatter/output/parser/OrFunction.java
@@ -1,6 +1,5 @@
 package com.sx4.bot.formatter.output.parser;
 
-import com.sx4.bot.formatter.output.annotation.AcceptNull;
 import com.sx4.bot.formatter.output.function.FormatterEvent;
 import com.sx4.bot.formatter.output.function.FormatterFunction;
 
@@ -10,8 +9,8 @@ public class OrFunction extends FormatterFunction<Boolean> {
 		super(Boolean.class, "or", "Combines with another boolean and returns true if either are true otherwise false");
 	}
 
-	public Boolean parse(FormatterEvent<Boolean> event, @AcceptNull Boolean bool) {
-		return bool == null ? event.getObject() : bool || event.getObject();
+	public Boolean parse(FormatterEvent<Boolean> event, Boolean bool) {
+		return bool || event.getObject();
 	}
 
 }
